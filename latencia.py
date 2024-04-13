a7d8ef4dcefeb6e3bf9aba72f0cd5524439f0d1e
TheDeveloperEver <76601635+thedeveloperever@users.noreply.github.com>
2024-04-12 23:59:56 -0400

    Forgot to change input to int (i learned python 2 so its separate)

--- latencia.py
+++ latencia.py
@@ -16,25 +16,29 @@ print("\t 1. Input Latency")
 print("\t 2. FPS")
 print("\t 3. Networking")
 
-titlechoice = input("Enter your choice [1, 2, 3] :: ")
+titlechoice = int(input("Enter your choice [1, 2, 3] :: "))
+os.system("cls")
 
 if titlechoice == 1:
     print(inputtitle)
     print("\t 1. Disable Extra Devices")
     print("\t 2. Disable Mouse Acceleration")
     print("\t 3. Change Win32PrioritySeparation")
-    inputchoice = input("Enter your choice [1, 2, 3] :: ")
+    inputchoice = int(input("Enter your choice [1, 2, 3] :: "))
+    os.system("cls")
 
 if titlechoice == 2:
     print(fpstitle)
     print("\t 1. Disable Extra Services")
     print("\t 2. Optimize Windows Settings")
     print("\t 3. Disable Defender")
-    fpschoice = input("Enter your choice [1, 2, 3] :: ")
+    fpschoice = int(input("Enter your choice [1, 2, 3] :: "))
+    os.system("cls")
 
 if titlechoice == 3:
     print(nettitle)
     print("\t 1. Optimize Ethernet Adapter")
     print("\t 2. Optimize WiFi Adapter")
     print("\t 3. Optimize NETSH Settings")
-    netchoice = input("Enter your choice [1, 2, 3] :: ")
+    netchoice = int(input("Enter your choice [1, 2, 3] :: "))
+    os.system("cls")
