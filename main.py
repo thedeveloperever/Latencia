5c444fc7d8f8adb105c0ec17952b5cdf631ccee7
TheDeveloperEver <levilouis@protonmail.com>
2024-11-22 22:07:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 import pyfiglet
 import os
 
-import latency
-import fps
-import networking
+import tweaks
 
-title = pyfiglet.figlet_format("Latencia")
+def menu():
+    print(pyfiglet.figlet_format("Latencia"))
+    print("\t 1. Input Latency")
+    print("\t 2. FPS")
+    print("\t 3. Networking")
+    print("\t 4. Exit")
+    choice = int(input("Enter your choice [1, 2, 3, 4] :: "))
+    os.system("cls")
 
-print(title)
-print("\t 1. Input Latency")
-print("\t 2. FPS")
-print("\t 3. Networking")
-choice = int(input("Enter your choice [1, 2, 3] :: "))
-os.system("cls")
+    if choice == 1:
+        tweaks.latency()
+    if choice == 2:
+        tweaks.fps()
+    if choice == 3:
+        tweaks.networking()
+    if choice == 4:
+        os._exit()
+    if choice != 1 and != 2 and !=3 and !=4:
+        print("\t Invalid Choice")
 
-if choice == 1:
-    latency()
-if choice == 2:
-    fps()
-if choice == 3:
-    networking()
+menu()
